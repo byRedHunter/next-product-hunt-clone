04560dc3611f70d64c576852d2579445de128094
Jhonny Quispe <jhonny.quispejl@gmail.com>
2021-08-05 21:56:56 -0500

    integrar firebase

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyDZ78dGBWCaXLqHbbBvE2gtUIS9QZipsIw',
+	authDomain: 'react-product-hunt-8b4e0.firebaseapp.com',
+	projectId: 'react-product-hunt-8b4e0',
+	storageBucket: 'react-product-hunt-8b4e0.appspot.com',
+	messagingSenderId: '64555305046',
+	appId: '1:64555305046:web:e9a07333b468e6ad3993f7',
+}
+
+export default firebaseConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import FirebaseContext from '../firebase/FirebaseContext'
+import firebase from '../firebase/firebase'
 import '../styles/app.css'
 
-function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />
+const MyApp = (props) => {
+	const { Component, pageProps } = props
+
+	return (
+		<FirebaseContext.Provider value={{ firebase }}>
+			<Component {...pageProps} />
+		</FirebaseContext.Provider>
+	)
 }
 
 export default MyApp
