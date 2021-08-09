6ce5571b83a482533d8cbe4c8c5e83c7bb74504a
Jhonny Quispe <jhonny.quispejl@gmail.com>
2021-08-09 09:31:12 -0500

    listar productos

---
+++ components/layout/DetallesProducto.js
@@ -0,0 +1,140 @@
+import styled from '@emotion/styled'
+import React from 'react'
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+import { es } from 'date-fns/locale'
+import { css } from '@emotion/react'
+
+const Producto = styled.li`
+	padding: 1rem;
+	display: flex;
+	flex-direction: column;
+	border-bottom: 0.1rem solid var(--naranja);
+	row-gap: 2rem;
+	@media screen and (min-width: 700px) {
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		padding: 2rem 1rem;
+	}
+`
+const DescripcionProducto = styled.div`
+	display: grid;
+	gap: 2rem;
+	@media screen and (min-width: 700px) {
+		flex: 0 1 600px;
+		grid-template-columns: 1fr 3fr;
+	}
+`
+const Comentarios = styled.div`
+	margin: 2rem 0;
+	display: flex;
+	align-items: center;
+	div {
+		display: flex;
+		align-items: center;
+		border: 0.1rem solid var(--naranja);
+		padding: 0.3rem 1rem;
+		margin-right: 2rem;
+	}
+	img {
+		width: 2rem;
+		margin-right: 1rem;
+	}
+	p {
+		font-size: 1.6rem;
+		font-weight: 700;
+		&::last-of-type {
+			margin: 0;
+		}
+	}
+`
+const Imagen = styled.img`
+	border-radius: 0.3rem;
+	width: 100%;
+	@media screen and (min-width: 700px) {
+		width: 20rem;
+	}
+`
+const Votos = styled.div`
+	text-align: center;
+	border: 0.1rem solid var(--naranja);
+	display: flex;
+	justify-content: center;
+	padding: 1rem 0;
+	@media screen and (min-width: 700px) {
+		flex-direction: column;
+		padding: 3rem 2rem;
+		flex: 0 0 auto;
+	}
+
+	div {
+		font-size: 2rem;
+	}
+
+	p {
+		margin: 0;
+		font-size: 2rem;
+		font-weight: 700;
+	}
+`
+const Titulo = styled.h1`
+	font-size: 2rem;
+	font-weight: bold;
+	margin: 0;
+	cursor: pointer;
+`
+
+export const DetallesProducto = ({ product }) => {
+	const {
+		id,
+		comentarios,
+		creado,
+		descripcion,
+		empresa,
+		producto,
+		url,
+		urlImagen,
+		votos,
+	} = product
+
+	return (
+		<Producto>
+			<DescripcionProducto>
+				<div>
+					<Imagen src={urlImagen} alt={producto} />
+				</div>
+
+				<div>
+					<Titulo>{producto}</Titulo>
+					<p
+						css={css`
+							color: #555;
+						`}
+					>
+						{descripcion}
+					</p>
+
+					<Comentarios>
+						<div>
+							<img
+								src='/static/images/comentario.png'
+								alt='Icono de comentario'
+							/>
+							<p> {comentarios.length} Comentarios</p>
+						</div>
+					</Comentarios>
+
+					<p>
+						Publicado hace:{' '}
+						{formatDistanceToNow(new Date(creado), { locale: es })}
+					</p>
+				</div>
+			</DescripcionProducto>
+
+			<Votos>
+				<div>&#9650;</div>
+				<p> {votos} </p>
+			</Votos>
+		</Producto>
+	)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,45 @@
+import { useContext, useEffect, useState } from 'react'
+import { DetallesProducto } from '../components/layout/DetallesProducto'
 import Layout from '../components/layout/Layout'
+import FirebaseContext from '../firebase/FirebaseContext'
 
 export default function Home() {
+	const [productos, setProductos] = useState([])
+	const { clientFirebase } = useContext(FirebaseContext)
+
+	useEffect(() => {
+		const obtenerProdutos = () => {
+			clientFirebase.db
+				.collection('productos')
+				.orderBy('creado', 'desc')
+				.onSnapshot(manejarSnapshot)
+		}
+
+		obtenerProdutos()
+	}, [])
+
+	function manejarSnapshot(snapshot) {
+		const productos = snapshot.docs.map((doc) => {
+			return {
+				id: doc.id,
+				...doc.data(),
+			}
+		})
+
+		setProductos(productos)
+	}
+
 	return (
 		<Layout>
-			<h1>Home</h1>
+			<div className='contenedor'>
+				<div className='productos-parent'>
+					{productos.map((producto) => (
+						<ul key={producto.id}>
+							<DetallesProducto product={producto} />
+						</ul>
+					))}
+				</div>
+			</div>
 		</Layout>
 	)
 }
